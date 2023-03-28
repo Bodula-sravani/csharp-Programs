122f362e5df677d2c1a4d484a1e709e8142214bc
Bodula-sravani <sravanibodula@gmail.com>
2023-03-28 19:34:27 +0530

    Main metod

---
+++ mainClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class mainClass
+    {
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int[,] a = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            p.spiralMatrix(a);
+            int[] price = { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine($"Max profit {p.stock(price)}");
+
+            rotate r = new rotate();
+            r.rotate90();
+            
+
+        }
+    }
+}
