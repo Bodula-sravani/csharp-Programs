fd911522963551cd8bd5b4b21a7ca4584bba3cee
Bodula-sravani <sravanibodula@gmail.com>
2023-03-31 14:41:35 +0530

    PhotoBook program and unit testing

---
+++ PhotoBook/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PhotoBook
+{
+    public class PhotoBookClass
+    {
+
+        public int album;
+        public PhotoBookClass() 
+        {
+            this.album = 16; ;
+            Console.WriteLine("in default constructor");
+        }
+
+        public PhotoBookClass(int pages)
+        {
+            this.album=pages;
+            Console.WriteLine("in parameterised constructor");
+        }
+        public int GetnumberPages()
+        {
+            return this.album;
+        }
+        static void Main(string[] args)
+        {
+            PhotoBookClass p = new PhotoBookClass();  //default constructor
+           Console.WriteLine($"Total no of pages: {p.GetnumberPages()}");
+
+        }
+    }
+
+    public class AddPhotoBook:PhotoBookClass
+    {
+        public AddPhotoBook()
+        {
+
+            this.album = 64;
+        }
+
+    }
+}
---
+++ PhotoBookTests/photoBookTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBook.Tests
+{
+    [TestClass()]
+    public class phoneBookTests
+    {
+        [TestMethod()]
+        public void defaultConstructorTest()
+        {
+            PhotoBookClass p = new PhotoBookClass();
+
+            if (p.GetnumberPages() == 16)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            { Assert.Fail(); }
+        }
+
+        [TestMethod()]
+        public void constructorTest()
+        {
+            int pages = 32;
+            PhotoBookClass p = new PhotoBookClass(pages);
+            if (p.GetnumberPages() == 32)
+            {
+                Assert.IsTrue(true);
+            }
+            else { Assert.Fail(); }
+        }
+
+        [TestMethod()]
+        public void AddPhoneBookTest()
+        {
+            AddPhotoBook p = new AddPhotoBook();
+            if (p.GetnumberPages() == 64)
+            {
+                Assert.IsTrue(true);
+            }
+
+            else { Assert.Fail(); }
+        }
+    }
+}
